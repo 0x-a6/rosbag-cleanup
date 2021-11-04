57b55caf2e1b83f2bed6d99cc8b98b231e1223a7
John Grant <john@smartmachine.nz>
2021-11-04 15:21:04 +1300

    Working through issues with re-timing, combing bag

--- bm3.py
+++ bm3.py
@@ -10,7 +10,7 @@ import os
 
 ''' 
 Bag Merge for python 3
-Designed for combining anynumber of python bags
+Designed for combining anynumber of ROS bags
 Reindexing time to be continuous
 '''
 
@@ -18,6 +18,8 @@ class Bag:
     '''
     Bag Class to store location and details about the bag
     '''
+    
+        
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -25,15 +27,28 @@ def parse_args():
         description='Merges bagfiles.')
     parser.add_argument('-o', type=str, help='name of the output file', 
         default = None, metavar = "output_file")
-    parser.add_argument('-t', type=str, help='topics which should be merged to the main bag', 
-        default = None, metavar = "topics")
+    # topic merging ... leaving it here doing nothing with it
+    # parser.add_argument('-t', type=str, help='topics which should be merged to the main bag', 
+        # default = None, metavar = "topics")
     parser.add_argument('-i', help='reindex bagfile', 
         default = False, action="store_true")
-    parser.add_argument('bagfile', type=str, help='path to a bagfile which should be merged to the main bagfile')
+    parser.add_argument('bagfiles', nargs='+', help='path to a bagfile which should be merged to the main bagfile')
     args = parser.parse_args()
+    full_paths = [os.path.join(os.getcwd(), path) for path in args.bagfiles]
+    args.bagfiles = set()
+    for path in full_paths:
+        if os.path.isfile(path):
+            args.bagfiles.add(path)
     return args
 
 if __name__ == "__main__":
     args = parse_args()
-    args.t = args.t.split(',') if args.t != None else args.t
+    # topic merging ... leaving it here doing nothing with it
+    # args.t = args.t.split(',') if args.t != None else args.t
+    
+    
+    
+    for f in args.bagfiles:
+        print(f)
+
     
